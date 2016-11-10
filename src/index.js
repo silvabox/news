ef0964a3101536817f346364dbb3514868fb5d6b
Darrenf <darrenf54321@gmail.com>
2016-11-10 15:25:36 +0000

    article getter model added, index.js updated, failing as loading Object not article

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 list = new List();
+articleGetter = new ArticleGetter();
+
+console.log(articleGetter.getArticle().returnText());
+
+list.addArticle(articleGetter.getArticle().returnText())
 list.addArticle("Trump wins election")
 list.addArticle("Winter is coming")
+
+
 
 articleController = new ArticleController(list);
 articleController.insertHTML();
