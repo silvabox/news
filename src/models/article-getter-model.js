8bb5e2814e2b419bf8196d2e1fbf751af888073d
B-raw <michaelbarry1989@hotmail.com>
2016-11-13 02:02:50 +0000

    all views working

--- src/models/article-getter-model.js
+++ src/models/article-getter-model.js
@@ -8,22 +8,24 @@
       if (this.readyState == 4 && this.status == 200) {
         guardianArticleObject = JSON.parse(xhr.response)
         guardianArticleArray = guardianArticleObject.response.results
-        console.log(guardianArticleArray)
-        return guardianArticleArray.map(function(guardianArticle) {
+        articleArray = guardianArticleArray.map(function(guardianArticle) {
           articleUrl = guardianArticle.webUrl;
-          apiUrl = guardianArticle.apiUrl
+          apiUrl = guardianArticle.apiUrl.replace(/s/, "")
           title = guardianArticle.webTitle;
           pubDate = guardianArticle.webPublicationDate
           articleBody = summarizeBody(apiUrl);
           return new Article(title, articleUrl, apiUrl, pubDate, articleBody);
         })
+        return articleArray;
       }
     };
     xhr.open("GET", "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?section=uk-news&page-size=5", false);
     xhr.send();
+    return articleArray
   };
 
   function summarizeBody(apiUrl) {
+    var artBody;
     var xhttp = new XMLHttpRequest();
     aylienUrl = "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=" + apiUrl + "?show-fields=body";
     xhttp.onreadystatechange = function() {
@@ -31,12 +33,12 @@
         // Action to be performed when the document is read;
         summaryObject = JSON.parse(xhttp.response);
         articleBody = summaryObject.response.content.fields.body
-        console.log("summarize Body =" + articleBody)
-        return articleBody
+        return articleBody;
       }
     };
-    xhttp.open("GET", aylienUrl, true);
+    xhttp.open("GET", aylienUrl, false);
     xhttp.send();
+    return articleBody;
   };
 
   exports.ArticleGetter = ArticleGetter;
